a03c0fc68708166af1a666afb9cb07c34036538f
Surya Vishakan <suryavishakan@Mac.lan>
2025-07-17 10:20:25 -0500

    updated navbar spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <nav className="bg-[#101212] w-full py-5">
        
-       <div className='container mx-auto'>
+       <div className='px-4 container mx-auto'>
              <img
-        className='w-auto h-10'
+        className='w-auto h-8 md:h-10'
         src = {logo} alt = 'greenheap enterprise logo' />
        </div>
        
